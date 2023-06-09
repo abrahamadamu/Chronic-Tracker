2d9dad5838876709f1e55d2a7ab03696692ae062
Abraham <meetabrahamadamu@gmail.com>
2023-06-09 10:15:56 -0300

    Initialize firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+import { onRequest } from "firebase-functions/v2/https";
+import * as express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Working</h1>");
+});
+
+export const helloWorld = onRequest(app);
